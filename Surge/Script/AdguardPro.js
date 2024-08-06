dd6449cd433e8a456d315b6bb67dab15154da0bc
Irrucky <152947891+Irrucky@users.noreply.github.com>
2024-08-06 15:18:53 +0800

    更新 AdguardPro.js

--- Surge/Script/AdguardPro.js
+++ Surge/Script/AdguardPro.js
@@ -1,19 +1,10 @@
 /******************************
-
-脚本功能：AdGuard 解锁永久高级版
-软件版本：4.4.5
-下载地址：http://t.cn/A6Kmswld
-脚本作者：Passer_by_yun
-更新时间：2022-12-26
-电报频道：https://t.me/yqc_123
-问题反馈：https://t.me/yqc_777
-使用声明：⚠️仅供学习交流，🈲️商业用途
-
+author：Passer_by_yun
 *******************************
 
 [rewrite_local]
 # > AdGuard 解锁永久高级版
-^https?:\/\/mobile-api\.adguard\.org\/api\/.+\/ios_validate_receipt url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/AdGuardProCrack.js
+^https?:\/\/mobile-api\.adguard\.org\/api\/.+\/ios_validate_receipt url script-response-body https://raw.githubusercontent.com/Irrucky/Tool/main/Surge/Script/AdguardPro.js
 
 [mitm] 
 hostname = *.adguard.org
